b23a66378a07c8172853fa8e18b546b88c8e4677
Elon Gustavo <systematicallyinit@gmail.com>
2026-09-11 21:55:21 +0100

    Reload page after user logout
    Added window.location.reload() to refresh the page after logout.

--- app/dashboard/components/DesktopSideBar.js
+++ app/dashboard/components/DesktopSideBar.js
@@ -25,6 +25,7 @@ export const DesktopSideBar = ({tab}) => {
   
           // Refresh the Next.js router/cache
           router.refresh();
+            window.location.reload();
         }
       } catch (error) {
         console.error("Logout error:", error);
