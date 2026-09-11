ed76b4485627192fc463bb96a7d11d3ae9c42978
Gustavo-Akajiaku <techsunbird@gmail.com>
2026-09-11 16:39:36 +0100

    v1.0.9 live support

--- app/layout.js
+++ app/layout.js
@@ -97,7 +97,7 @@ export default function RootLayout({ children }) {
         <Providers>
           {children}
         </Providers>
-        
+        <LiveChat />
       </body>
     </html>
   );
